a4f825517f225d53727dfe0c46b1e0247f6762f0
Андрей <andrew_k19@mail.ru>
2022-08-26 09:26:14 +0700

    Initial commit

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,20 @@
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class AppConfig {
+ 
+    @Bean(name="helloworld")
+    public HelloWorld getHelloWorld() {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setMessage("Hello World!");
+        return helloWorld;
+    }
+    @Bean(name = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    @Scope("prototype")
+    public Cat getCat() {
+        return new Cat();
+    }
+}
